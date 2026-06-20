384743e1d5fddebc2e8da828c042c757f80471d9
Kesha <t1sh3happy@gmail.com>
2026-06-20 19:04:56 +0300

    Подготовил решение KTB-05

--- app/src/main/kotlin/KTB-02-print-dictionary.kt
+++ app/src/main/kotlin/KTB-02-print-dictionary.kt
@@ -1,26 +1,48 @@
 import java.io.File
 
+const val ERROR_CONDITION = -1
 
 fun main() {
 
-    val wordsFile: File = File("words.txt")
+//    val wordsFile: File = File("words.txt")
 //    wordsFile.createNewFile()
 //    wordsFile.writeText("hello привет\n")
 //    wordsFile.appendText("dog собака\n")
 //    wordsFile.appendText("cat кошка\n")
-    val dictionary = mutableListOf<Word>()
-    val lines: List<String> = wordsFile.readLines()
+//    val dictionary = mutableListOf<Word>()
+//    val lines: List<String> = wordsFile.readLines()
+//
+//    for (line in lines) {
+//        val parts = line.split("|")
+//        val word =
+//            Word(text = parts[0], translate = parts[1], correctAnswersCount = parts.getOrNull(2)?.toIntOrNull() ?: 0)
+//        dictionary.add(word)
+//    }
+//    println(dictionary)
+    while (true) {
+        println(
+            "Меню: \n" +
+                    "1 – Учить слова\n" +
+                    "2 – Статистика\n" +
+                    "0 – Выход\n" +
+                    "Введите цифру: "
+        )
+        val state: Int = readln().toIntOrNull() ?: ERROR_CONDITION
+        when (state) {
+            0 -> {
+                println("Завершение работы")
+                break
+            }
 
-    for (line in lines) {
-        val line = line.split("|")
-        val word = Word(text = line[0], traslate = line[1], correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0)
-        dictionary.add(word)
+            1 -> println("Учить слова")
+            2 -> println("Статистика")
+            else -> println("Неверный ввод")
+        }
     }
-    println(dictionary)
 }
 
-data class Word(
-    val text: String,
-    val traslate: String,
-    var correctAnswersCount: Int = 0,
-)
+//data class Word(
+//    val text: String,
+//    val translate: String,
+//    var correctAnswersCount: Int = 0,
+//)
