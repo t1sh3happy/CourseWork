d91c6d9ab68922dc213e9dc2dd2121b88ffc72b9
Kesha <t1sh3happy@gmail.com>
2026-07-25 09:50:35 +0300

    Подготовил решение KTB-12

--- app/src/main/kotlin/Telegram.kt
+++ app/src/main/kotlin/Telegram.kt
@@ -3,11 +3,13 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val TELEGRAM_BASE_URL = "https://api.telegram.org/bot"
+
 fun main(args: Array<String>) {
 
     val botToken: String = args[0]
-    val urlGetMe: String = "https://api.telegram.org/bot$botToken/getMe"
-    val urlGetUpdates: String = "https://api.telegram.org/bot$botToken/getUpdates"
+    val urlGetMe: String = TELEGRAM_BASE_URL + botToken + "/getMe"
+    val urlGetUpdates: String = TELEGRAM_BASE_URL + botToken + "/getUpdates"
 
     val client0: HttpClient = HttpClient.newBuilder().build()
     val request0: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
