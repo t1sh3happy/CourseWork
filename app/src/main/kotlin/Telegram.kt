3b3faec63b73aeaeb32fd77e198a625f534f4fa1
Kesha <t1sh3happy@gmail.com>
2026-07-29 23:25:26 +0300

    Подготовил решение KTB-15

--- app/src/main/kotlin/Telegram.kt
+++ app/src/main/kotlin/Telegram.kt
@@ -1,7 +1,3 @@
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
 
 const val TELEGRAM_BASE_URL = "https://api.telegram.org/bot"
 
@@ -11,30 +7,23 @@ fun main(args: Array<String>) {
     var updateId = 0
     val updateIdRegex = Regex(""""update_id":\s*(\d+)""")
     val messageRegex = Regex("\"text\":\"(.+?)\"")
+    val chatIdRegex = Regex(""""id":\s*(\d+)""")
 
 
     while (true) {
         Thread.sleep(2000)
-        val updates : String = getUpdates(botToken, updateId)
+        val updates: String = getUpdates(botToken, updateId)
         val searchebleId = updateIdRegex.find(updates)?.groups?.get(1)?.value ?: continue
         val message = messageRegex.find(updates)?.groups?.get(1)?.value ?: continue
-        println(message)
-        println(searchebleId)
+        val chatId = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toInt() ?: continue
+        val text: String
+
+        if (message == "Hello") {
+            text = "Hello"
+            sendMessage(botToken, chatId, text)
+        }
 
         updateId = searchebleId.toInt() + 1
 
-
     }
 }
-
-
-fun getUpdates(botToken: String, updateId: Int): String {
-    val urlGetUpdates: String = TELEGRAM_BASE_URL + botToken + "/getUpdates?offset=$updateId"
-    val client1: HttpClient = HttpClient.newBuilder().build()
-    val request1: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-    val response1: HttpResponse<String> = client1.send(request1, HttpResponse.BodyHandlers.ofString())
-    return response1.body()
-}
-
-
-
---
+++ app/src/main/kotlin/TelegramBotService.kt
@@ -0,0 +1,19 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun getUpdates(botToken: String, updateId: Int): String {
+    val urlGetUpdates: String = TELEGRAM_BASE_URL + botToken + "/getUpdates?offset=$updateId"
+    val client1: HttpClient = HttpClient.newBuilder().build()
+    val request1: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val response1: HttpResponse<String> = client1.send(request1, HttpResponse.BodyHandlers.ofString())
+    return response1.body()
+}
+
+fun sendMessage(botToken: String, chatId: Int, text: String) {
+    val urlSendMessage: String = TELEGRAM_BASE_URL + botToken + "/sendMessage?chat_id=$chatId&text=$text!"
+    val client2: HttpClient = HttpClient.newBuilder().build()
+    val request2: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
+    val response2: HttpResponse<String> = client2.send(request2, HttpResponse.BodyHandlers.ofString())
+}
