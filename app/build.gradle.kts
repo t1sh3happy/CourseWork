fe6eee79fb9bd5551bd10c6d06ddb39bfbb8d0ab
Kesha <t1sh3happy@gmail.com>
2026-08-20 00:27:41 +0300

    Подготовил решение KTB-21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,14 @@
 plugins {
-    // Apply the shared build logic from a convention plugin.
-    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
     id("buildsrc.convention.kotlin-jvm")
-
-    // Apply the Application plugin to add support for building an executable JVM application.
+    kotlin("plugin.serialization") version "2.0.21"  // <-- УКАЖИТЕ ВАШУ ВЕРСИЮ KOTLIN
     application
 }
 
 dependencies {
-    // Project "app" depends on project "utils". (Project paths are separated with ":", so ":utils" refers to the top-level "utils" project.)
     implementation(project(":utils"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
 }
 
 application {
-    // Define the Fully Qualified Name for the application main class
-    // (Note that Kotlin compiles `App.kt` to a class with FQN `com.example.app.AppKt`.)
     mainClass = "com.automation.app.AppKt"
 }
